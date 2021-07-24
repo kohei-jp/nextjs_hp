ef566fe4268414cbef238457207a312af7cf3355
kohei-jp <norwaymi38@gmail.com>
2021-07-24 14:34:23 +0900

    get Static Props

---
+++ components/Post.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Post({ post }) {
+  return (
+    <div>
+      <span>{post.id}</span>
+      {" : "}
+      <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
+        {post.title}
+      </span>
+    </div>
+  );
+}
